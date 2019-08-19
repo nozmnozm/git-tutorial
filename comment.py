9d839bb069c6d5c9ffa738c0ae4d497d27631b30
Nozomi Noz <kynozminz@gmail.com>
2019-08-19 23:11:27 +0900

    New File comment.py First Commit
    Commit Test

---
+++ comment.py
@@ -0,0 +1,22 @@
+#comment.py
+str_single = 'シングルクォートで"文字列"を作成できます。'
+str_dubble = "ダブルクオートで'文字列'を作成できます。"
+str_triple1 = '''
+            改行付きで
+            文字列を書くことが
+            できます。
+            '''
+str_triple2 = """
+            ダブルクオートでも
+            改行付きで
+            文字列を
+            書くことができます。
+            """
+
+print(str_single)
+print()
+print(str_dubble)
+print()
+print(str_triple1)
+print()
+print(str_triple2)
